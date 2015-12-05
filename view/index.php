420da5ad888607e1b83d46e466b4c32fa9eb180e
Александр Зозуленко <oduvanio@mail.ru>
2015-12-05 10:17:49 +0400

    Попытка работать с путями для показа картинки в новостях

--- view/index.php
+++ view/index.php
@@ -4,8 +4,8 @@
 	<title>Новости</title>
 </head>
 <body>
-	<?php foreach($news as $news_one): ?>
-		<img src="<?php echo $news_one['title']; ?>" style="float: left; margin-right: 10px;">
+	<?php foreach($news as $news_one): echo realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $news_one['picture']);?>
+		<img src="<?php echo realpath(__DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'images' . DIRECTORY_SEPARATOR . $news_one['picture']);?>" style="float: left; margin-right: 10px;">
 		<h2><?php echo $news_one['title']; ?></h2>
 		<p><?php echo $news_one['description']; ?></p>
 		<a href="controller/article.php?id=<?php echo $news_one['id']; ?>">Страница новости</a>
