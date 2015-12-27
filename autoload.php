e4358a17fe088b6afa43e44f543c27dccef7bde1
Александр Зозуленко <oduvanio@mail.ru>
2015-12-28 00:02:33 +0400

    update

--- autoload.php
+++ autoload.php
@@ -1,11 +1,11 @@
 <?php
 function __autoload($class)
 {
-	if (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
-		require __DIR__ . '/controllers/' . $class . '.php';
-	} elseif (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
-		require __DIR__ . '/models/' . $class . '.php';
-	} elseif (file_exists(__DIR__ . '/controllers/' . $class . '.php')) {
+	if (file_exists(__DIR__ . '/controller/' . $class . '.php')) {
+		require __DIR__ . '/controller/' . $class . '.php';
+	} elseif (file_exists(__DIR__ . '/model/' . $class . '.php')) {
+		require __DIR__ . '/model/' . $class . '.php';
+	} elseif (file_exists(__DIR__ . '/classes/' . $class . '.php')) {
 		require __DIR__ . '/classes/' . $class . '.php';
 	}	
 }
--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/autoload.php';
 
-$ctrl = isset($_GET['ctrl'] ? $_GET['ctrl'] : 'News');
-$act = isset($_GET['act'] ? $_GET['act'] : 'All');
+$ctrl = isset($_GET['ctrl']) ? $_GET['ctrl'] : 'News';
+$act = isset($_GET['act']) ? $_GET['act'] : 'All';
 $controllerClassName = $ctrl . 'Controller';
 require_once __DIR__ . '/controller/' . $controllerClassName . '.php';
 $controller = new $controllerClassName;
