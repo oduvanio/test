0d0acde1815f26def492c1d67bcf201c17560352
Александр Зозуленко <oduvanio@mail.ru>
2016-01-02 11:30:35 +0400

    продолжаем кодить

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -1,19 +1,27 @@
 <?php
-class NewsController
+class AdminController
 {
 	public function actionAll()
 	{
 		$items = News::getAll();
-		include __DIR__ . '/../view/news/all.php';
+		include __DIR__ . '/../view/news/newsAll.php';
 	}
 	public function actionOne()
 	{
 		$id = $_GET['id'];
 		$items = News::getOne($id);
-		include __DIR__ . '/../view/news/one.php';
+		include __DIR__ . '/../view/news/newsOne.php';
 	}
-	public function actionAddOne()
+	public function actionAdd()
 	{
-		
+		include __DIR__ . '/../view/form_add_news.php';
+	}
+	public function addOne()
+	{
+		$title = $_POST['title'];
+		$description = $_POST['description'];
+		$picture = $_POST['image'];
+		News::addOne($title, $description, $picture);
+		$this->actionAll();
 	}
 }
--- model/News.php
+++ model/News.php
@@ -4,6 +4,7 @@ class News extends AbstractModel
 	public $id;
 	public $title;
 	public $text;
+	public $picture;
 	protected static $table = 'news';
 	protected static $class = 'News';
 }
